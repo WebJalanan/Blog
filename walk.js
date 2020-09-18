808887f5b1be19854d174bd52b46a1f775bfd42f
WebJalanan <WebJalanan@users.noreply.github.com>
2020-09-18 21:58:37 +0700

    Create walk.js

---
+++ walk.js
@@ -0,0 +1,3 @@
+var dv = document.createElement('div'); 
+    dv.innerHTML = "<div id=\"walk\"><img src=\"https://1.bp.blogspot.com/-H34hn5Fjf5w/X2Qu5dmn7oI/AAAAAAAAFOU/O_iAWrpIzMMn0T8zWZEZ0M7dhufadY6rgCLcBGAsYHQ/s16000/bigwalk.gif\"></div><style>#walk{bottom:-5px; position:fixed;-webkit-animation:linear infinite;-webkit-animation-name: run;-webkit-animation-duration: 50s;}@-webkit-keyframes run {0% { right: 0;} 50%{ right : 100%;} 100%{ right: 100%;}}</style>"; 
+document.body.appendChild(dv);
